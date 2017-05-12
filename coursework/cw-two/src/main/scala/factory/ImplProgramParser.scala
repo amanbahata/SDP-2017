2094bf96043cfa90cd2ea4f3f7e1c1b3797268e9
Aman Bahata <bahataaman@gmail.com>
2017-05-12 17:00:11 +0100

    Implements parse() and parseString() methods

--- coursework/cw-two/src/main/scala/factory/ImplProgramParser.scala
+++ coursework/cw-two/src/main/scala/factory/ImplProgramParser.scala
@@ -1,6 +1,8 @@
 package factory
 
-import vendor.ProgramParser
+import vendor.{Instruction, ProgramParser}
+
+import scala.io.Source
 
 /**
   * Created by aman1 on 12/05/2017.
@@ -13,7 +15,14 @@ class ImplProgramParser extends ProgramParser {
     * @param file the file to parse
     * @return an instruction list
     */
-  override def parse(file: String): InstructionList = ???
+  override def parse(file: String): InstructionList = {
+    val fileLines = Source.fromFile(file).getLines()
+    var instructionsList = Vector[Instruction]()
+    for (line <- fileLines)
+      instructionsList = instructionsList ++: parseString(line)
+
+    instructionsList
+  }
 
   /**
     * Parses a string representation of a bytecode program
@@ -22,5 +31,19 @@ class ImplProgramParser extends ProgramParser {
     * @param string the string to parse
     * @return an instruction list
     */
-  override def parseString(string: String): InstructionList = ???
+  override def parseString(string: String): InstructionList = {
+    // Stores the contents of each line in the program
+    //    val lineContainer: Array[String] = string.split("\n")
+    var instructionsList = Vector[Instruction]()
+    var number = Vector[Int]()
+
+    var splitLine = string.split(" ")
+    var operand = splitLine(0)
+    if (splitLine.length > 1) {
+      val num = splitLine(1).toInt
+      number = number :+ num
+    }
+    instructionsList = instructionsList :+ new Instruction(operand, number)
+    instructionsList
+  }
 }

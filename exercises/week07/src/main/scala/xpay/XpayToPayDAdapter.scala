e4dfaff189e453a4c786eb6831e8ef8b898817b1
Aman Bahata <bahataaman@gmail.com>
2017-05-14 17:03:17 +0100

    Adds xpay exercise for week07 in adaptor pattern

---
+++ exercises/week07/src/main/scala/xpay/XpayToPayDAdapter.scala
@@ -0,0 +1,33 @@
+package xpay
+
+/**
+  * Created by aman1 on 14/05/2017.
+  */
+class XpayToPayDAdapter(xpay: Xpay) extends PayD{
+
+  override def getCustCardNo: String = xpay.getCreditCardNo
+
+  override def setCustCardNo(custCardNo: String): Unit = xpay.setCreditCardNo(custCardNo)
+
+  override def getCardOwnerName: String = xpay.getCustomerName
+
+  override def setCardOwnerName(cardOwnerName: String): Unit = xpay.setCustomerName(cardOwnerName)
+
+  override def getCardExpMonthDate: String = xpay.getCardExpMonth + "/" + xpay.getCardExpYear
+
+  override def setCardExpMonthDate(cardExpMonthDate: String): Unit = formatDate(cardExpMonthDate)
+
+  override def getCVVNo: Integer = xpay.getCardCVVNo.toInt
+
+  override def setCVVNo(cVVNo: Integer): Unit = xpay.setCardCVVNo(cVVNo.toShort)
+
+  override def getTotalAmount: Double = xpay.getAmount
+
+  override def setTotalAmount(totalAmount: Double): Unit = xpay.setAmount(totalAmount)
+
+  private def formatDate(cardExpMonthDate: String) = {
+      val md = cardExpMonthDate.split("/")
+      xpay.setCardExpMonth(md(0))
+      xpay.setCardExpYear(md(1))
+  }
+}
